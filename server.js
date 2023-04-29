44179f84c941f8e6242b1c9d079addec81393019
Shinde Rahul <shinde2732000@gmail.com>
2023-04-29 05:35:02 +0530

    project modified for socket.io server compatibility

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
 const port = 3000;
+const io = require("socket.io")(server);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.listen(port, () => {
+  console.log("Hello World!");
+});
